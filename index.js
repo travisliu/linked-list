eb08466e8b1208d7d91e843d8829adf75c9741c0
travisliu <travisliu@users.noreply.github.com>
2022-01-16 10:00:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,156 @@
+class Node {
+  #value = null;
+  #next = null;
+
+  constructor(value, next = null) {
+    this.#value = value;
+    this.#next  = next;
+  }
+
+  get next() {
+    return this.#next;
+  }
+
+  set next(next) {
+    this.#next = next;
+  }
+
+  get value() {
+    return this.#value;
+  }
+
+  set value(value) {
+    this.#value = value;
+  }
+}
+
+class Iterator {
+  #currentNode;
+  #hasNext;
+
+  constructor(node) {
+    this.#currentNode = node;
+    this.#hasNext = !!node.next;
+  }
+
+  get hasNext() { 
+    return this.#hasNext;
+  }
+  
+  next() {
+    if (!this.#hasNext) return null;
+    
+    const currentValue = this.#currentNode.value;
+    this.#currentNode = this.#currentNode.next;
+    this.#hasNext = !!this.#currentNode;
+    
+    return currentValue;
+  }
+}
+
+class List {
+  #head = null;
+  #tail = null;
+  #size = null;
+
+  constructor(head) {
+    this.#size = 0;
+
+    this.#initialize(head);
+  }
+
+  #initialize(head) {
+    this.#head = head;
+
+    let node = head;
+    while(node) {
+      this.#size++;
+
+      if (!node.next) {
+        this.#tail = node;
+        break;
+      }
+
+      node = node.next;
+    }
+  }
+
+  get head() {
+    return this.#head;
+  }
+
+  get tail() {
+    return this.#tail;
+  }
+
+  get size() {
+    return this.#size;
+  }
+
+  generateIterator() {
+    return new Iterator(this.#head);
+  }
+
+  prepend(node) {
+    const temp = this.#head;
+    this.#head = node;
+    
+    if (temp) {
+      this.#head.next = temp;
+    } else {
+      this.#tail = this.#head;
+    }
+    
+    this.#size += 1;
+  }
+
+  append(node) {
+    if (!this.#head) {
+      this.prepend(node);
+      return
+    }
+    
+    this.#size += 1;
+    if (this.#tail) this.#tail.next = node;
+    this.#tail = node;
+  }
+
+  remove(index) {
+    if (this.#size === 0) return;
+    if (index > this.#size - 1) return;
+    
+    let targetNode = null;
+    let previousNode = null;
+ 
+    if (index === 0) {
+      targetNode = this.#head;
+      this.#head = targetNode.next;
+    } else {
+      previousNode = this.find(index - 1);
+      targetNode = previousNode.next;
+      previousNode.next = targetNode.next;
+    }
+      
+    if (this.#tail === targetNode) {
+      this.#tail = previousNode;
+    }
+    
+    this.#size -= 1;
+  }
+
+  find(index) {
+    let currentNode = this.#head;
+    for (let i = 1; i <= index; i++) {
+      if (currentNode.next === null) return null;
+      
+      currentNode = currentNode.next
+    }
+    
+    return currentNode;
+  } 
+}
+
+module.exports = {
+  Node,
+  List
+}
---
+++ spec.js
@@ -0,0 +1,251 @@
+const { Node, List } = require('./index.js');
+
+describe('List', () => {
+  subject(() => new List($headNode))
+
+  def('headNode', () => null)
+  def('head', () => $subject.head)
+  def('tail', () => $subject.tail)
+
+  describe('.constructor', () => {
+    def('headNode', () => createNodeBySize($nodeAmount))
+
+    describe('when head node is null', () => {
+      def('nodeAmount', () => 0)
+
+      its('size', () => is.expected.toBe(0))
+      its('tail', () => is.expected.toBeNull())
+    })
+
+    describe('when head node is alone', () => {
+      def('nodeAmount', () => 1)
+
+      its('size', () => is.expected.toBe($nodeAmount))
+      its('tail', () => is.expected.toBe($head))
+    })
+
+    describe('when head node has 3 nodes linked', () => {
+      def('nodeAmount', () => 3)
+
+      its('size', () => is.expected.toBe($nodeAmount))
+      it(() => is.expected.toHaveProperty('tail.value', $nodeAmount))
+    })
+  })
+
+  describe('.find', () => {
+    def('headNode', () => createNodeBySize(6))
+
+    describe('when index is pointed to head', () => {
+      def('index', () => 0)
+
+      it('returns the head node', () => {
+        expect($subject.find($index)).toBe($head);
+      })
+    })
+
+    describe('when index is pointed to tail', () => {
+      def('index', () => 5)
+
+      it('returns the tail node', () => {
+        expect($subject.find($index)).toBe($tail);
+      })
+    })
+
+    describe('when index is pointed to middle node', () => {
+      def('index', () => 3)
+
+      it('returns the node be pointed with index', () => {
+        expect($subject.find($index).value).toBe($index + 1);
+      })
+    })
+  })
+
+  describe('.prepend', () => {
+    def('newNode', () => new Node(9))
+
+    beforeEach(() => {
+      $subject.prepend($newNode);
+    })
+
+    describe('when head node is null', () => {
+      def('headNode', () => null)
+
+      its('size', () => is.expected.toBe(1))
+      it(() => is.expected.toHaveProperty('head', $newNode))
+      it(() => is.expected.toHaveProperty('tail', $newNode))
+    })
+
+    describe('when head node is alone', () => {
+      def('headNode', () => createNodeBySize(1))
+
+      its('size', () => is.expected.toBe(2))
+      it(() => is.expected.toHaveProperty('head', $newNode))
+ 
+      it('retains the original tail', () => {
+        expect($tail.value).toBe(1);
+      })
+    })
+
+    describe('when head node has 3 nodes linked', () => {
+      def('headNode', () => createNodeBySize(3))
+
+      its('size', () => is.expected.toBe(4))
+      it(() => is.expected.toHaveProperty('head', $newNode))
+
+      it('retains the original tail', () => {
+        expect($tail.value).toBe(3);
+      })
+    })
+  })
+
+  describe('.append', () => {
+    def('newNode', () => new Node(9))
+
+    beforeEach(() => {
+      $subject.append($newNode);
+    })
+
+    describe('when head node is null', () => {
+      def('headNode', () => null)
+
+      its('size', () => is.expected.toBe(1))
+      its('head', () => is.expected.toBe($newNode))
+      its('tail', () => is.expected.toBe($newNode))
+    })
+
+    describe('when head node is alone', () => {
+      def('headNode', () => createNodeBySize(1))
+
+      its('size', () => is.expected.toBe(2))
+      its('tail', () => is.expected.toBe($newNode))
+
+      it('retains the original head', () => {
+        expect($head.value).toBe(1);
+      })
+    })
+
+    describe('when head node has 3 nodes linked', () => {
+      def('headNode', () => createNodeBySize(3))
+
+      its('size', () => is.expected.toBe(4))
+      its('tail', () => is.expected.toBe($newNode))
+      
+      it('retains the original head', () => {
+        expect($head.value).toBe(1);
+      })
+    })
+  })
+
+  describe('.remove', () => {
+    describe('when head node is alone', () => {
+      def('headNode', () => createNodeBySize(1))
+
+      beforeEach(() => {
+        $subject.remove(0);
+      })
+
+      its('size', () => is.expected.toBe(0))
+      its('head', () => is.expected.toBeNull())
+      its('tail', () => is.expected.toBeNull())
+    })
+
+    describe('when head node has 6 nodes linked', () => {
+      def('headNode', () => createNodeBySize(6))
+
+      beforeEach(() => {
+        $subject.remove($index);
+      })
+
+      describe('and index is pointed to the head node', () => {
+        def('index', () => 0)
+
+        its('size', () => is.expected.toBe(5))
+
+        it('assigns second node as new head node', () => {
+          expect($head.value).toBe(2);
+        })
+
+        it('retains the original tail', () => {
+          expect($tail.value).toBe(6);
+        })
+      })
+
+      describe('and index is pointed the middle node', () => {
+        def('index', () => 3)
+
+        its('size', () => is.expected.toBe(5))
+
+        it('replaces with next node', () => {
+          expect($subject.find($index).value).toBe($index + 2);
+        })
+
+        it('retains the original head', () => {
+          expect($head.value).toBe(1);
+        })
+
+        it('retains the original tail', () => {
+          expect($tail.value).toBe(6);
+        })
+      })
+
+      describe('and index is pointed the tail node', () => {
+        def('index', () => 5)
+
+        its('size', () => is.expected.toBe(5))
+
+        it('replaces tail with previous node', () => {
+          expect($tail.value).toBe($index);
+        })
+
+        it('retains the original head', () => {
+          expect($head.value).toBe(1);
+        })
+      })
+    })
+  })
+
+  describe('.generateIterator', () => {
+    subject('iterator', () => $subject.generateIterator())
+
+    describe('when head node is alone', () => {
+      def('headNode', () => createNodeBySize(1))
+
+      its('hasNext', () => is.expected.toBeFalsy())
+      its('next', () => is.expected.toBeNull())
+    })
+
+    describe('when head node has 3 nodes linked', () => {
+      def('headNode', () => createNodeBySize(3))
+
+      it('returns a iterator can be moved to next', () => {
+        expect($iterator.hasNext).toBeTruthy();
+        expect($iterator.next()).toBe(1);
+        expect($iterator.hasNext).toBeTruthy();
+        expect($iterator.next()).toBe(2);
+        expect($iterator.hasNext).toBeTruthy();
+        expect($iterator.next()).toBe(3);
+        expect($iterator.hasNext).toBeFalsy();
+        expect($iterator.next()).toBeNull();
+      })
+    })
+  })
+})
+
+const createNodeBySize = size => {
+  let headNode = null;
+  let previousNode = null;
+
+  for (let i = 1; i <= size; i++) {
+    const currentNode = new Node(i);
+
+    if (headNode == null) { 
+      headNode = currentNode;
+    } else {
+      previousNode.next = currentNode;
+    }
+
+    previousNode = currentNode;
+  }
+
+  return headNode;
+}
